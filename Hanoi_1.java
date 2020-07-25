0ab09b4a251a24095534b88a0037e87de8bf853f
guillem2604 <45925851+guillem2604@users.noreply.github.com>
2020-07-25 09:58:36 -0700

    Create Hanoi_1.java

---
+++ Hanoi_1.java
@@ -0,0 +1,17 @@
+class Hanoi_1 {
+  public static void main(String[] args) {
+    System.out.println("Hanoi de 5 discos es: \n"+ Main.hanoi (1,2,3,3));
+  }
+
+  public static String hanoi (int ini, int aux, int fin, int cantDiscos)
+  {
+    if (cantDiscos == 1)
+      return "Desde "+ini+" hasta "+fin+ "\n";
+
+    String sal  = hanoi (ini, fin, aux, cantDiscos-1);
+           sal += hanoi (ini, aux, fin, 1);
+           sal += hanoi (aux, ini, fin, cantDiscos-1);
+
+    return sal;
+  }
+}
